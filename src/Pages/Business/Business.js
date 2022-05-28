a0f489f7e95e26f0284caa5fe8d3b929a5ed6a64
raiansakil <aburaian182@gmail.com>
2022-05-28 10:31:44 +0600

    added icon

--- src/Pages/Business/Business.js
+++ src/Pages/Business/Business.js
@@ -1,3 +1,7 @@
+import { HiFlag } from "react-icons/hi";
+import { HiThumbUp } from "react-icons/hi";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { HiOutlineIdentification } from "react-icons/hi";
 import React from "react";
 
 const Business = () => {
@@ -10,20 +14,25 @@ const Business = () => {
       </div>
       <div class="stats stats-vertical lg:stats-horizontal shadow">
         <div class="stat">
+          <HiFlag className="h-10 w-10 ml-4 text-blue-500"></HiFlag>
+
           <div class="stat-title">Countries</div>
           <div class="stat-value">72</div>
         </div>
 
         <div class="stat">
+          <HiOutlineIdentification className="h-10 w-10 ml-4 text-blue-500"></HiOutlineIdentification>
           <div class="stat-title">Complete Projects</div>
           <div class="stat-value">535+</div>
         </div>
 
         <div class="stat">
+          <HiOutlineUserGroup className="h-10 w-10 ml-4 text-blue-500"></HiOutlineUserGroup>
           <div class="stat-title">Happy Clients</div>
           <div class="stat-value">280+</div>
         </div>
         <div class="stat">
+          <HiThumbUp className="h-10 w-10 ml-4 text-blue-500"></HiThumbUp>
           <div class="stat-title">Feedbacks</div>
           <div class="stat-value">432+</div>
         </div>
